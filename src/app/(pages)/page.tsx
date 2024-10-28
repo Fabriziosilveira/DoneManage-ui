0902ad69cffe5067138af629fdd7bee14c31c39c
Fabriziosilveira <silveira6602@gmail.com>
2024-10-28 17:00:27 -0300

    :wrench: chore: Add components and dependencies

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,9 +1,19 @@
-import LoginForm2 from "@/components/Login/LoginForm";
+// import LoginForm2 from "@/components/Login/LoginForm";
 //import QuadroKanban from "@/components/Notification/draggableKanban";
+
+//import { SidebarDemo } from "@/components/templates/Teste";
+
+// import Dashboard from "@/components/templates/Teste";
+
+//import { SidebarDemo } from "@/components/templates/Teste";
+
+
+import HomePage from "@/components/Home/Home";
+//import { BackgroundLinesDemo } from "@/components/templates/Teste";
 
 export default function Home() {
   return (
-      <LoginForm2 />
-      //<QuadroKanban />
+      <HomePage />
+      //<SidebarDemo />
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,15 +10,15 @@ const config: Config = {
   theme: {
   	extend: {
   		colors: {
-			custom: {
-				black: '#111111',
-				white: '#FFFFFF',
-				base: '#F8F9FC',
-				secondary: '#F1F3F9',
-				blackLine: '#3F444D',
-				neutralLine: '#E1E6EF',
-				notificationRed: '#E92C2C',
-			},
+  			custom: {
+  				black: '#111111',
+  				white: '#FFFFFF',
+  				base: '#F8F9FC',
+  				secondary: '#F1F3F9',
+  				blackLine: '#3F444D',
+  				neutralLine: '#E1E6EF',
+  				notificationRed: '#E92C2C'
+  			},
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
@@ -54,6 +54,16 @@ const config: Config = {
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
   			}
   		},
   		borderRadius: {
